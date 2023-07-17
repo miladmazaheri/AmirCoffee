e813eaf4564d09867cbb34fdb45d1cb3f8eeebaf
Milad Mazaheri <milad.mazaheri@dayasoft.com>
2023-07-17 17:54:41 +0330

    Add project files.

---
+++ AmirCoffee.Web/Pages/Privacy.cshtml.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace AmirCoffee.Web.Pages
+{
+    public class PrivacyModel : PageModel
+    {
+        private readonly ILogger<PrivacyModel> _logger;
+        public string DateStr { get; set; }
+
+        public PrivacyModel(ILogger<PrivacyModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+            DateStr = DateTime.Now.ToString("yyyy-MM-dd");
+        }
+    }
+}
